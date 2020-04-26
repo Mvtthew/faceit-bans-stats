99e9926dcf0db1f412a95b06052e3ff886c6a829
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-04-26 11:07:33 +0200

    :pencil2: Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 // Material
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [
@@ -17,7 +19,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-		MatToolbarModule
+		MatToolbarModule,
+		MatButtonModule,
+		MatIconModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
