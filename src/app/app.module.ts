fbb883b495ca6317cb84323810bfd5c16e7df545
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-04-26 10:56:25 +0200

    :pencil2: Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+
+// Material
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		ToolbarComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatToolbarModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
